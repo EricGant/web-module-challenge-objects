14d5afbc5d37eed0aa41a3158a073e06745c57a0
Eric <egant43123@gmail.com>
2021-06-10 11:59:59 -0400

    was accidentally pushing to the wrong branch. fixed now, and almost done

--- index.js
+++ index.js
@@ -15,9 +15,11 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
+function createMenuItem(name, price, category){
+  return {name, price, category}
 }
+
+createMenuItem('tacos', 8, 'Lunch')
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Invoke your function!
@@ -28,6 +30,11 @@ Test your createMenuItems function by doing the following:
   
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
+console.log('task 1b', createMenuItem('tacos', 8, 'Lunch'));
+console.log('task 1b', createMenuItem('burritos', 10, 'Dinner'));
+console.log('task 1b', createMenuItem('Chicken', 10, 'Lunch'));
+console.log('task 1b', createMenuItem('Pizza', 15, 'Dinner'));
+console.log('task 1b', createMenuItem('Burger', 9, 'Lunch'));
 
 
 
@@ -48,10 +55,13 @@ export const burger = {
   name: "Burger", 
   price: 18, 
   category: "Lunch", 
-  /*Your code here*/
+  discount : function(p){
+    if(p === "teacher" || p ==="student"){
+  return (this.price*.75)
+    } else {return this.price*.90}
+    }
 }
-
-
+console.log('task 2', burger.discount("teacher"))
 
 ///////////////Reviews (MVP)///////////////////
 const reviews = [
@@ -69,6 +79,7 @@ const reviews = [
 Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
+console.log('task 3',reviews[5])
 
 
 
@@ -78,7 +89,8 @@ Using the reviews array above do the following: (no function needed)
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
-
+reviews.push({name: "Eric", rating: 10, feedback: "Eric is the best"})
+console.log('task 4', reviews)
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -87,7 +99,8 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   2. log the reviews array to the console to check your work
 */
 
-
+reviews[7].feedback = "this place is chill with really cool people, great for getting work done on weekdays"
+console.log('task 5', reviews)
 
 
 
@@ -102,9 +115,11 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
+function getReviewByIndex(a, n) {
+  return `${a[n].name} gave the restaurant a ${a[n].rating} star review, and their feedback was: ${a[n].feedback}`
 }
+getReviewByIndex(reviews,0)
+
 
 
   
